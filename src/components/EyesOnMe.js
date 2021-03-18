c0f5760dc2bd5e1552956f61cc1dd389135c04c3
syd333 <sydmiranda@icloud.com>
2021-03-18 06:21:35 -0700

    commit tree 5ef61817aedff395432c1e91f06a562374c601eb

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render(){
+        return(
+            <div>
+                <button onFocus={(e) => {
+                    console.log('Good!')}}
+                        onBlur={(e) => {
+                            console.log('Hey! Eyes on me!')}}>       
+                    button</button>
+            </div>
+        )
+    }
+
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+import React from 'react'
+
+class Keypad extends React.Component {
+constructor(){
+    super()
+}
+
+    render(){
+        return(
+            <div>
+                <input onKeyUp={(e) => {
+                    console.log('Entering password...')}} type="password"></input>
+            </div>
+        )
+    }
+}
+
+export default Keypad;
